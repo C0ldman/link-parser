d588c9946fe8cd059f17107bfec9826e10a7d1c5
Y.Ukrainets <y.ukrainets@viseven.com>
2021-06-04 15:53:12 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -6,28 +6,25 @@ window.addEventListener('DOMContentLoaded', parse);
 function parse() {
     if (!window.location.search) return
 
-    let params = new URLSearchParams(window.location.href);
-    let searchParameters = getParameters(params);
+    let searchParameters = getParameters();
 
     let links = document.links;
 
     for (let element of links) {
         if (element.href.startsWith(subdomain)) {
             let newLink = getLink(element, searchParameters);
-            updateElementLink(element, newLink)
+            element.setAttribute('href', newLink);
         }
     }
 }
 
-function getParameters(incomeParams) {
-    let params = [];
-    for (let item of incomeParams) {
-        let key = item[0].toLowerCase().replace(`${window.location.origin}${window.location.pathname}?`, "");
-        if (key.startsWith(startKey)) {
-            params = [...params, [`${key}`, `${item[1]}`]];
-        }
-    }
-    return params
+function getParameters() {
+    let searchString = window.location.search.slice(1);
+    return searchString
+        .split('&')
+        .map(item => item.split('='))
+        .map(item=>[item[0].toLowerCase(),item[1]])
+        .filter(item=>item[0].startsWith(startKey));
 }
 
 function getLink(element, params) {
@@ -36,9 +33,3 @@ function getLink(element, params) {
     params.forEach(item => newLink = `${newLink}${item[0]}=${item[1]}&`);
     return newLink.slice(0, -1)
 }
-
-function updateElementLink(element, link) {
-    element.setAttribute('href', link);
-}
-
-modules.exports = parse;
