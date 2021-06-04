5ac0bc59efff19ad65e5725d89767d87b8d646e6
Y.Ukrainets <y.ukrainets@viseven.com>
2021-06-04 15:07:40 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
+const startKey = 'abc_';
+const subdomain = 'https://subdomain.example.com'
+
 window.addEventListener('DOMContentLoaded', parse);
 
-function parse(e) {
+function parse() {
     if (!window.location.search) return
 
-    let subdomain = 'https://subdomain.example.com'
     let params = new URLSearchParams(window.location.href);
-    let search = getParameters(params);
+    let searchParameters = getParameters(params);
 
     let links = document.links;
 
     for (let element of links) {
         if (element.href.startsWith(subdomain)) {
-            updateLink(element, search)
+            let newLink = getLink(element, searchParameters);
+            updateElementLink(element, newLink)
         }
     }
 }
@@ -20,21 +23,22 @@ function getParameters(incomeParams) {
     let params = [];
     for (let item of incomeParams) {
         let key = item[0].toLowerCase().replace(`${window.location.origin}${window.location.pathname}?`, "");
-        if (key.startsWith('abc_')) {
-            params.push([`${key}`, `${item[1]}`]);
+        if (key.startsWith(startKey)) {
+            params = [...params, [`${key}`, `${item[1]}`]];
         }
     }
     return params
 }
 
-function updateLink(element, params) {
-
+function getLink(element, params) {
     let hasQueryParams = element.href.includes("?");
-    let newLink  = hasQueryParams ? element.href + "&" : element.href + "?";
-
-    for (let item of params) {
-        newLink = `${newLink}${item[0]}=${item[1]}&`
-    }
-
-    element.setAttribute('href', newLink.slice(0, -1))
+    let newLink = hasQueryParams ? element.href + "&" : element.href + "?";
+    params.forEach(item => newLink = `${newLink}${item[0]}=${item[1]}&`);
+    return newLink.slice(0, -1)
 }
+
+function updateElementLink(element, link) {
+    element.setAttribute('href', link);
+}
+
+modules.exports = parse;
